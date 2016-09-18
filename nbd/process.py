e4c332b5821e8b57c3e9044ba44aaea70dddda44
a@a.com <a@a.com>
2016-09-18 13:24:53 -0500

    auto

--- nbd/process.py
+++ nbd/process.py
@@ -1,13 +1,14 @@
-inf = open('test.py')
-outf = open('test2.py', 'w')
+inf = open('nbd3.tex')
+outf = open('test2_gen.py', 'w')
 outf.truncate()
+outf.write('o = open("nbd3_gen.tex", "w")\n')
+outf.write('o.truncate()\n')
 
 for line in inf:
     line = line[:-1]
     line = line.replace('\\','\\\\')
-    outf.write("print '" + line + "' \n")
+    outf.write("o.write('" + line + "\\n') \n")
 
-
-
-
-
+outf.write('o.close()\n')
+inf.close()
+outf.close()
