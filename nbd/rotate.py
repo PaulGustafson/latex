a5169e4aea19c11ad8bdfbf845bf42c475d58a21
a@a.com <a@a.com>
2016-09-24 20:45:17 -0500

    auto

--- nbd/rotate.py
+++ nbd/rotate.py
@@ -1,4 +1,4 @@
-# Argument: list of strings
+# Argument: list of strings labelling edges incident to a vertex
 # Returns: LaTeX of associators used for 1 left rotation
 # Side Effect: rotates input list
 # Example: (a,b,c) \mapsto (c,a,b)
@@ -9,28 +9,27 @@ def rotateL(edges):
 def reassociateL(edges):
     if (len(edges) < 3):
         return ""
-    return wi(edges[0], cat(edges[1:-1]), edges[-1]) + reassociate(edges[0:-1])
+    return wi(edges[0], cat(edges[1:-1]), edges[-1]) + reassociateL(edges[0:-1])
 
-# Argument: list of strings
+# Argument: list of strings labelling edges incident to a vertex
 # Returns: LaTeX of associators used for 1 right rotation
 # Side Effect: rotates input list
 # Example: (a,b,c) \mapsto (b,c,a)
-def rotateL(edges):
-    ans =  reassociateR(edges)
+def rotateR(edges):
+    ans =  reassociateR(edges, 2)
     edges.append(edges.pop(0))
     return ans
 
-def reassociateL(edges):
-    if (len(edges) < 3):
+def reassociateR(edges, n):
+    if (n >= len(edges)):
         return ""
-    return wi(edges[0], cat(edges[1:-1]), edges[-1]) + reassociate(edges[0:-1])
-
-
-
+    return w(edges[0], cat(edges[1:n]), edges[n]) + reassociateR(edges, n + 1)
 
 def wi(a,b,c):
     return "\omega^{-1}(" + a + ", " + b + ", " + c + ") "
 
+def w(a,b,c):
+    return "\omega(" + a + ", " + b + ", " + c + ") "
 
 def cat(edges):
     ans = ""
@@ -42,7 +41,14 @@ def cat(edges):
 # Rotate main vertex edges for composition
 edges = ['h','g','h^{-1}', 'hg^{-1}h^{-1}']
 print(rotateL(edges) + rotateL(edges))
+print(edges) 
+
+# Apply composition to ['h^{-1}', 'hg^{-1}h^{-1}', 'h', 'g'] 
+edges.append('g')
+edges.append('g^{-1}')
 print(edges)
 
-# After composition: (edges) \otimes (g g^{-1})
-print(wi(cat(edges), 'g', 'g^{-1}'))
+# After composition: (edges) \otimes (g g^{-1}), fix this
+print(wi(cat(edges[0:-2]), 'g', 'g^{-1}'))
+print(rotateR(edges) + rotateR(edges))
+print(edges)
