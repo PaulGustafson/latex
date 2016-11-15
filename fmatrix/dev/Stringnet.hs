6997225dbbdad1aaab37b2d38ec6cb059ab188c5
a@a.com <a@a.com>
2016-11-15 14:24:15 -0600

    auto

--- fmatrix/dev/Stringnet.hs
+++ fmatrix/dev/Stringnet.hs
@@ -1,3 +1,14 @@
-type Object = String
+-- Represent the graph using an adjacency matrix with entries in Obj(A)
+-- This matrix should be equal to its conjugate transpose.
+-- We also need to keep track of the order of edges out of each vertex
+-- along with the parenthesization.  This should be represented by a
+-- binary tree whose leaves are the outgoing edges.
 
-type Stringnet = [[Objects]]
+import Data.Graph
+
+
+label :: Vertex -> Morphism
+
+
+
+fnet :: Stringnet
