2fcfd7e07df8d85fd73087ae26f2d60e93790f91
a@a.com <a@a.com>
2016-12-15 15:07:55 -0600

    auto

--- hask/stringnet.hs
+++ hask/stringnet.hs
@@ -1,34 +1,35 @@
 
+
+-- need isotopy information.  Encode as triangulation?
+
 data AObject =  g | h | k | l | One | Inverse AObject | Product AObject AObject
 
 type AMorphism = phi | Id AObject | Coev AObject | Ev AObject | Prod AMorphism AMorphism
 
-
-type Vertex = Main | LeftHole | RightHole | Top1 | Top2 | Top3 | Top4
+data Vertex = Main | LeftHole | RightHole | Top1 | Top2 | Top3 | Top4
 
 -- Multiple edges between two nodes are enumerated in
 -- CCW order from the start node, starting at the eastward ray
 data Edge = Edge { start :: Vertex
                  , end   :: Vertex
-                 , index :: Integer
+                 , nextEdge :: Maybe Edge
                  } deriving (Show)
 
 
-data Stringnet = Stringnet { --outgoingEdges :: Vertex -> [Edge]
+data Stringnet = Stringnet { 
                             vLabel  :: Vertex -> AMorphism
                            , eLabel  :: Edge -> AObject
                            } 
 
 
 -- replace the starting vertex with the composition, make the end vertex empty
--- Is there a way to intermediate pattern-match edge = (Edge start end index) ?
 compose :: Stringnet -> Edge -> Stringnet
-compose sn edge@(Edge start end index) =  Stringnet vLabel2 eLabel2
-  where vLabel2 start = Prod (vLabel start) (Prod (Ev $ Inverse $ eLabel edge) (vLabel end))
+compose (Stringnet vLabel eLabel) edge@(Edge composeStart end nextEdge) =  Stringnet vLabel2 eLabel2
+  where vLabel2 composeStart = Prod (vLabel composeStart) (Prod (Ev $ Inverse $ eLabel edge) (vLabel end))
         vLabel2 end   = One
-        vlabel2 other = vlabel other
-        eLabel2 (Edge start end _ ) = One
-        elabel2 
+        vLabel2 other = vlabel other
+        eLabel2 (Edge _ end _ )  = One  -- kill all edges into endNode 
+        eLabel2 (Edge composeStart _ _) = eLabel 
 
                
         
