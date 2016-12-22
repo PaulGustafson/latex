f1f3eba78b17cd79981c1aa2a1faa72d7fa707d1
a@a.com <a@a.com>
2016-12-21 20:30:25 -0600

    auto

--- hask/stringnet.agda
+++
@@ -1,3 +0,0 @@
-module stringnet where
-  
-  data
--- hask/stringnet.hs
+++ hask/stringnet.hs
@@ -1,24 +1,29 @@
 -- Encode as marked CW-complex.
 
-data AObject  =  G | H | K | L | One | Inverse AObject | Product AObject AObject
 
-data AMorphism = Phi | Id AObject | Coev AObject | Ev AObject | Prod AMorphism AMorphism
+data InitialVertex = Main | LeftPuncture | RightPuncture deriving (Enum)
 
-type Vertex = Integer
+data InitialEdge = LeftLoop | RightLoop | LeftLeg | RightLeg deriving (Enum)
 
-type EdgeIndex = Integer  -- unique id for each edge, not the array index in OneComplex
+data AObject  =  G InitialEdge | One | Inverse AObject | Product AObject AObject
 
--- change into a record?
+data AMorphism = Phi InitialVertex | Id AObject | Coev AObject | Ev AObject | Prod AMorphism AMorphism
+
+-- TODO: Change to newtype and fix the "toEnum" stuff
+type Vertex = Int
+
+type EdgeIndex = Int  -- unique id for each edge, not the array index in OneComplex
+
 type Edge = ([Vertex], EdgeIndex)
 
 type Disk = [Edge] -- a cycle, i.e. snd edge1 = fst edge2 ..
 
 validDisk :: Disk -> Bool
 validDisk [] = False
-validDisk es = and $ zipWith (==)  [vs !! 1 | (vs, _) <- es] [vs !! 0 | (vs, _) <- tail $ cycles es]
+validDisk es = and $ zipWith (==)  [vs !! 1 | (vs, _) <- es] [vs !! 0 | (vs, _) <- tail $ cycle es]
 
 -- Consider changing this to a map Fin n -> Edge
-data OneComplex = [Edge]
+type OneComplex = [Edge]
 
 data Orientation = Plus | Minus
 
@@ -28,20 +33,20 @@ toDisk :: OneComplex -> AttachingMap -> Disk
 toDisk oc am = [oc !! i | (i,o) <- am]
 
 validAttachingMap :: OneComplex -> AttachingMap -> Bool
-validAttachingMap oc am = (and [ i < length oc | (i,o) <- am]) && validDisk $ toDisk oc am
+validAttachingMap oc am = (and [ i < length oc | (i,o) <- am]) && (validDisk $ toDisk oc am)
 
-data TwoComplex = TwoComplex { oneComplex :: OneComplex,
+data TwoComplex = TwoComplex { oneComplex :: OneComplex
                              , attachingMaps :: [AttachingMap]
                              }
 
 validTwoComplex :: TwoComplex -> Bool
-validTwoComplex tc = and $ map (validAttachingMap $ oneCompex tc) $ attachingMaps tc
+validTwoComplex tc = and $ map (validAttachingMap $ oneComplex tc) $ attachingMaps tc
 
 -- label the complex
--- TODO: Make the pattern-matching exhaustive (get rid of Maybe)
+-- TODO: Make the label pattern-matching exhaustive 
 data Stringnet = Stringnet {twoComplex :: TwoComplex
-                           , edgeLabel :: Edge -> Maybe AObject,
-                           , vertexLabel :: Vertex -> Maybe AMorphism
+                           , edgeLabel :: Edge -> AObject
+                           , vertexLabel :: Vertex -> AMorphism
                            }
 
 -- TODO
@@ -51,11 +56,7 @@ data Stringnet = Stringnet {twoComplex :: TwoComplex
 -- validStringnet (tc@(oc, _), aos, label) = validTwoComplex tc && validEdgeLabelling oc aos
 
 
--- initial conditions
-data InitialVertex = Main | LeftPuncture | RightPuncture  deriving (Enum)
-
-data InitialEdge = LeftLoop | RightLoop | LeftLeg | RightLeg deriving (Enum)
-
+-- more initial conditions
 vertices :: InitialEdge -> [Vertex]
 vertices LeftLoop  = map fromEnum [Main, Main]
 vertices RightLoop = map fromEnum [Main, Main]
@@ -67,15 +68,26 @@ initialOneComplex :: OneComplex
 initialOneComplex = [(vertices ie, fromEnum ie) | ie <- [LeftLoop .. RightLeg]]
 
 initialTwoComplex :: TwoComplex
-initialTwoComplex = (initialOneComplex, map (map (fromEnum, id))
+initialTwoComplex = TwoComplex initialOneComplex $ map (map f)
                      [[(LeftLoop, Plus), (RightLoop, Plus)]
                      ,[(LeftLoop, Minus), (LeftLeg, Plus), (LeftLeg, Minus)]
                      ,[(RightLoop, Minus), (RightLeg, Plus), (RightLeg, Minus)]
-                     ])
+                     ]
+                    where f (a, b) = (fromEnum a, b)
 
-                    
+initialStringnet :: Stringnet
+initialStringnet = Stringnet { twoComplex  = initialTwoComplex
+                             , edgeLabel   = G . toEnum . snd
+                             , vertexLabel = Phi . toEnum
+                             }
 
-                    
 contract :: Stringnet -> EdgeIndex -> Stringnet
-contract sn i = Stringnet
+contract sn i = Stringnet { twoComplex = TwoComplex { oneComplex = oneComplex twoComplex sn
+                                                    , attachingMaps = d
+                                                    }                               
+                          , edgeLabel  = edgeLabel sn
+                          , vertexLabel = --FIXME vertexLabel sn
+                          }
+                
+                 
 
