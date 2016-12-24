0887c236d7da40852a1ff58e442428ed4742669c
Paul Gustafson <pgustafs@bloc506b-01.math.tamu.edu>
2016-12-23 19:00:35 -0600

    auto

--- hask/stringnet.hs
+++ hask/stringnet.hs
@@ -1,24 +1,32 @@
 -- Encode as marked CW-complex.
--- TODO: Make the label pattern-matching exhaustive
--- TODO: Refactor using Simplex n
+-- TODO: Finish computing TwoComplex transformations for figures
+-- TODO: Make Edge equality take disks into account.  Currently, two
+-- Connect Edges are equal if they have the same endpoints.  This is a bug.
+--
+-- Maybe: Refactor using Simplex n
 
 -- Left and right refer to positions before the braiding operation
--- Orientations are given by arrows in the paper
 data Vertex = Main | LeftPuncture | RightPuncture | Midpoint Edge | Contract Edge
+  deriving (Show, Eq)
 
+-- Orientations of initial edges are given by arrows in the figures in the paper
 data Edge = LeftLoop | RightLoop | LeftLeg | RightLeg -- initial edges
           | FirstHalf Edge | SecondHalf Edge -- result of adding coev vertex
           | Connect Vertex Vertex -- connecting with a 1 edge
           | TensorE Edge Edge     -- stick together parallel edges
           | ContractedNeighbor
-            { contractedNeighbor :: Edge
+            { contractedEdge :: Edge
             , preimage  :: Edge
-            , commonVertex :: Vertex  -- what if they share both endpoints? 
             }
-          deriving (Show)
+          deriving (Show, Eq)
 
-data Disk = OutsideDisk | LeftDisk | RightDisk | Cut Disk Vertex Vertex
-          deriving (Show)
+data TwoComplex = TwoComplex 
+                  { vertices :: [Vertex]
+                  , edges :: [Edge]
+                  }
+
+-- data Disk = OutsideDisk | LeftDisk | RightDisk | Cut Disk Vertex Vertex
+--           deriving (Show)
 
 data Object = G | H | K | L | One | Star Object | TensorO Object Object
 
@@ -28,14 +36,23 @@ data Morphism = Phi | Id AObject | Coev Object | Ev Object | TensorM Morphism Mo
 data Orientation = Plus | Minus
 
 endpoints :: Edge -> [Vertex]
-endpoints LeftLoop  = [Main, Main]
-endpoints RightLoop = [Main, Main]
-endpoints LeftLeg   = [Main, LeftPuncture]
-endpoints RightLeg  = [Main, RightPuncture]
-endpoints FirstHalf e = [(endpoints e) !! 0, Midpoint e]
-endpoints SecondHalf e = [Midpoint e, (endpoints e) !! 1]
-endpoints Connect v1 v2 = [v1, v2]
-endpoints TensorE e1 _ = endpoints e1
+let start e = (endpoints e) !! 0
+      end e = (endpoints e) !! 1
+      in
+  endpoints LeftLoop  = [Main, Main]
+  endpoints RightLoop = [Main, Main]
+  endpoints LeftLeg   = [Main, LeftPuncture]
+  endpoints RightLeg  = [Main, RightPuncture]
+  endpoints FirstHalf e = [(endpoints e) !! 0, Midpoint e]
+  endpoints SecondHalf e = [Midpoint e, (endpoints e) !! 1]
+  endpoints Connect v1 v2 = [v1, v2]
+  endpoints TensorE e1 _ = endpoints e1
+  endpoints ContractedNeighbor c p  = [if start p `elem` endpoints c
+                                        then Contract c
+                                        else start p
+                                      , if end p `elem` endpoints c
+                                        then Contract c
+                                        else end p
 
 start :: Edge -> Vertex
 start e = (endpoints e) !! 0
@@ -44,12 +61,12 @@ end :: Edge -> Vertex
 end e = (endpoints e) !! 1
 
 -- TODO: deal with edge compositions changing this function
-perimeter :: Disk -> [(Edge, Orientation)]
-perimeter OutsideDisk = [(LeftLoop, Plus), (RightLoop, Plus)]
-perimeter LeftDisk    = [(LeftLoop, Minus), (LeftLeg, Plus), (LeftLeg, Minus)]
-perimeter RightDisk   = [(RightLoop, Minus), (RightLeg, Plus), (RightLeg, Minus)]
-perimeter Cut d v1 v2 = [(Connect v2 v1, Plus)] ++ (takeWhile (f v2) dropWhile (f v1) $ cycle $ perimeter d) 
-                              where f v (e, o) = v != start e
+-- perimeter :: Disk -> [(Edge, Orientation)]
+-- perimeter OutsideDisk = [(LeftLoop, Plus), (RightLoop, Plus)]
+-- perimeter LeftDisk    = [(LeftLoop, Minus), (LeftLeg, Plus), (LeftLeg, Minus)]
+-- perimeter RightDisk   = [(RightLoop, Minus), (RightLeg, Plus), (RightLeg, Minus)]
+-- perimeter Cut d v1 v2 = [(Connect v2 v1, Plus)] ++ (takeWhile (f v2) dropWhile (f v1) $ cycle $ perimeter d) 
+--                               where f v (e, o) = v != start e
                                     
 objectLabel :: Edge -> Object
 objectLabel LeftLoop = G
@@ -67,8 +84,40 @@ morphismLabel RightPuncture = None
 morphismLabel Midpoint e = Just $ Coev e
 morphismLabel Contract e = Just $ Compose (Ev e) (TensorM (morphismLabel (start e)) (morphismLabel (end e)))
 
+touches :: Edge -> Edge -> Bool
+touches e1 e2 = if  length (intersect (endpoints e1) (endpoints e2)) > 0)
+                                        then True
+                                        else False
+
+contract :: Edge -> TwoComplex -> TwoComplex
+contract e tc = TwoComplex
+                { vertices = Contract e ++ [v | v <- vertices tc
+                                             , not $ v `elem` endpoints tc]
+                , edges = [ContractedNeighbor e p | p <- edges tc
+                                                  , e `touches` p
+                                                  , e != p]
+                          ++ [ e1 | e1 <- edges tc
+                                  , not (e `touches` p)
+                                  , e != p]
+                }
+
+connect :: Vertex -> Vertex -> TwoComplex -> TwoComplex
+connect v1 v2 tc = TwoComplex
+                   {  vertices = vertices tc
+                   ,  edges = Connect v1 v2 ++ edges tc
+                   }
+
+addCoev :: Edge -> TwoComplex -> TwoComplex
+addCoev e tc = TwoComplex
+               { vertices = [Midpoint e] ++ vertices tc
+               , edges = [FirstHalf e, SecondHalf e] ++ [f | f <- edges tc, f != e]
+               }
+  
 -- vX corresponds to figure number X from the paper
-v1 = [Main, LeftPuncture, RightPuncture]
+TwoComplex tc1 = TwoComplex
+                 { vertices =  [Main, LeftPuncture, RightPuncture]
+                 , edges =   [LeftLoop | RightLoop | LeftLeg | RightLeg]
+                 }
 v2 = v1
 -- topX means X from the top
 top1 = Midpoint LeftLoop
@@ -80,7 +129,8 @@ e12 = Connect top1 top2
 e23 = Connect top2 top3
 e34 = Connect top3 top4
 
-top12 = (Contract top1)
+top12V = (Contract top1)
+      
 
 
 
