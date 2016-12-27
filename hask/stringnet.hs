d283db93228e1a11744ef2dd16bb8d725993cf36
a@a.com <a@a.com>
2016-12-27 17:59:15 -0600

    auto

--- hask/stringnet.hs
+++ hask/stringnet.hs
@@ -1,3 +1,4 @@
+import           Control.Monad.State
 import           Data.List
 
 -- Encode as marked CW-complex.
@@ -5,7 +6,10 @@ import           Data.List
 -- TODO: Make Edge equality take disks into account.  Currently, two
 -- Connect Edges are equal if they have the same endpoints.  This is a bug.
 --
+-- TODO: Typify stack-like usage of vertices, edges
 -- IDEA: Refactor using Simplex n
+
+
 
 -- Left and right refer to positions before the braiding operation
 -- TODO: Separate into interior vertex and puncture types
@@ -17,45 +21,58 @@ data Edge = LeftLoop | RightLoop | LeftLeg | RightLeg -- initial edges
           | FirstHalf Edge | SecondHalf Edge -- result of adding coev vertex
           | Connect Vertex Vertex -- connecting with a 1 edge
           | TensorE Edge Edge     -- stick together parallel edges
-          | ContractedNeighbor
-            { contractedEdge :: Edge
-            , preimage       :: Edge
-            }
           deriving (Show, Eq)
-
-data TwoComplex = TwoComplex
-                  { vertices :: [Vertex]
-                  , edges    :: [Edge]
-                  } deriving (Show)
-
--- data Disk = OutsideDisk | LeftDisk | RightDisk | Cut Disk Vertex Vertex
---           deriving (Show)
-
-data Object = G | H | K | L | One | Star Object | TensorO Object Object
-
--- TODO: add typing to Compose
-data Morphism = Phi | Id Object | Coev Object | Ev Object | TensorM Morphism Morphism | Compose Morphism Morphism
 
 data Orientation = Plus | Minus
 
-endpoints :: Edge -> [Vertex]
-endpoints LeftLoop  = [Main, Main]
-endpoints RightLoop = [Main, Main]
-endpoints LeftLeg   = [Main, LeftPuncture]
-endpoints RightLeg  = [Main, RightPuncture]
-endpoints (FirstHalf e) = [(endpoints e) !! 0, Midpoint e]
-endpoints (SecondHalf e) = [Midpoint e, (endpoints e) !! 1]
-endpoints (Connect v1 v2) = [v1, v2]
-endpoints (TensorE e1 _) = endpoints e1
-endpoints (ContractedNeighbor c p)  =
-  [if ep `elem` endpoints c then Contract c else ep | ep <- endpoints p]
+data OrientedEdge = (Edge, Orientation)
 
+-- instance Show Edge where
+--   show LeftLoop = "LeftLoop"
+--   show RightLoop = "RightLoop"
+--   show LeftLeg = "LeftLeg"
+--   show RightLeg = "RightLeg"
+--   show (FirstHalf e) = show e ++ "0"
+--   show (SecondHalf e) = show e ++ "1"
+--   show (Connect v1 v2) = (show v1) ++ "-" ++ (show v2)
+--   show (TensorE e1 e2) = "(" ++  (show e1) ++ ")*(" ++ (show e2) ++ ")"
 
-start :: Edge -> Vertex
-start e = (endpoints e) !! 0
+data TwoComplex = TwoComplex
+                  { vertices :: [Vertex]
+                  , edges    :: [OrientedEdge]
+                  , image :: Vertex -> Vertex
+                  } 
 
-end :: Edge -> Vertex
-end e = (endpoints e) !! 1
+-- data Disk = OutsideDisk | LeftDisk | RightDisk | Cut Disk Vertex Vertex
+--           deriving (Show)
+data Object = G | H | K | L | One | Star Object | TensorO Object Object
+            deriving (Show)
+                                                           
+-- TODO: add typing to Compose
+data Morphism = Phi | Id Object | Coev Object | Ev Object | TensorM Morphism Morphism | Compose Morphism Morphism
+              deriving (Show)
+                                                                                                 
+
+initialEndpoints :: Edge -> [Vertex]
+initialEndpoints edge  = case edge of
+  LeftLoop  -> [Main, Main]
+  RightLoop -> [Main, Main]
+  LeftLeg   -> [Main, LeftPuncture]
+  RightLeg  -> [Main, RightPuncture]
+  FirstHalf e -> [(initialEndpoints e) !! 0, Midpoint e]
+  SecondHalf e -> [Midpoint e, (initialEndpoints e) !! 1]
+  Connect v1 v2 -> [v1, v2]
+  TensorE e1 _ -> initialEndpoints e1
+  Reverse e -> reverse $ initialEndpoints e
+
+endpoints :: Edge -> TwoComplex -> [Vertex]
+endpoints e tc = map (image tc) $ initialEndpoints e
+
+start :: OrientedEdge -> TwoComplex -> Vertex
+start (e,o) tc =  (endpoints e tc) !! (if o = Plus then 0 else 1)
+
+end :: OrientedEdge -> TwoComplex -> Vertex
+end (e,o) tc =  (endpoints e tc) !! (if o = Plus then 1 else 0)
 
 -- TODO: deal with edge compositions changing this function
 -- perimeter :: Disk -> [(Edge, Orientation)]
@@ -65,6 +82,7 @@ end e = (endpoints e) !! 1
 -- perimeter Cut d v1 v2 = [(Connect v2 v1, Plus)] ++ (takeWhile (f v2) dropWhile (f v1) $ cycle $ perimeter d)
 --                               where f v (e, o) = v /= start e
 
+
 objectLabel :: Edge -> Object
 objectLabel LeftLoop = G
 objectLabel LeftLeg = H
@@ -73,49 +91,85 @@ objectLabel RightLeg = L
 objectLabel (FirstHalf e) = objectLabel e
 objectLabel (SecondHalf e) = Star (objectLabel e)
 objectLabel (Connect v1 v2) = One
+objectLabel (TensorE e1 e2) = TensorO (objectLabel e1) (objectLabel e2)
+objectLabel (Reverse e)  = Star (objectLabel e)
 
 -- inexhaustive-matching  (Punctures don't have labels, c.f. comments at Vertex definition)
-morphismLabel :: Vertex -> Morphism
+morphismLabel :: Vertex -> TwoComplex -> Morphism
 -- morphismLabel LeftPuncture = Nothing
 -- morphismLabel RightPuncture = Nothing
-morphismLabel Main = Phi
-morphismLabel (Midpoint e) = Coev $ objectLabel e
-morphismLabel (Contract e) = Compose (Ev $ objectLabel e) (TensorM (morphismLabel (start e)) (morphismLabel (end e)))
+morphismLabel Main _ = Phi
+morphismLabel (Midpoint e) _ = Coev $ objectLabel e
+morphismLabel (Contract e) tc = Compose (Ev $ objectLabel e)
+                                (TensorM (morphismLabel (start e tc) tc)
+                                 (morphismLabel (end e tc) tc))
 
-touches :: Edge -> Edge -> Bool
-touches e1 e2 = if  length (intersect (endpoints e1) (endpoints e2)) > 0
-                                        then True
-                                        else False
+tensor :: Edge -> Edge -> State TwoComplex Edge
+tensor e1 e2 = state $ \tc -> let product = TensorE e1 e2 in
+  (product, tc { edges = [product] ++ [e | e <- edges tc, e /= e1, e /= e2] })
 
-contract :: Edge -> TwoComplex -> TwoComplex
-contract contractedEdge tc = TwoComplex
-                { vertices = [Contract contractedEdge] ++ [v | v <- vertices tc, not $ v `elem` (endpoints contractedEdge)]
-                , edges = [if contractedEdge `touches` e
-                           then ContractedNeighbor contractedEdge e
-                           else e
-                          | e <- edges tc, contractedEdge /= e]
+
+contract :: Edge -> State TwoComplex Vertex
+contract contractedEdge  = state $ \tc ->
+  let composition = Contract contractedEdge in
+  (composition, TwoComplex
+                { vertices = [composition] ++
+                             [v | v <- vertices tc
+                                , not $ v `elem` (endpoints contractedEdge tc)]
+                , edges = [e | e <- edges tc
+                             , contractedEdge /= e]
+                , image = (\v -> if (v `elem` (endpoints contractedEdge tc))
+                                 then composition
+                                 else v
+                          ) . (image tc)
+                            
                 }
+  )
 
-connect :: Vertex -> Vertex -> TwoComplex -> TwoComplex
-connect v1 v2 tc = TwoComplex
-                   {  vertices = vertices tc
-                   ,  edges = [Connect v1 v2] ++ edges tc
-                   }
+connect :: Vertex -> Vertex -> State TwoComplex Edge
+connect v1 v2  = state $ \tc -> 
+  let connection = Connect v1 v2 in
+  ( connection, tc { edges = [connection] ++ edges tc} )
 
-addCoev :: Edge -> TwoComplex -> TwoComplex
-addCoev e tc = TwoComplex
-               { vertices = [Midpoint e] ++ vertices tc
-               , edges = [FirstHalf e, SecondHalf e] ++ [f | f <- edges tc, f /= e]
-               }
+addCoev :: Edge -> State TwoComplex (Vertex, Edge, Edge)
+addCoev e = state $ \tc ->
+  let v  = Midpoint e
+      fh = FirstHalf e
+      sh = SecondHalf e in
+  ((v, fh, sh), tc 
+                { vertices =  [v] ++ vertices tc
+                , edges = [fh, sh] ++ [f | f <- edges tc
+                                         , f /= e]
+                }
+  )
 
 -- tcX corresponds to figure number X from the paper
-tc1 = TwoComplex { vertices = [Main, LeftPuncture, RightPuncture]
-                 , edges    = [LeftLoop, RightLoop, LeftLeg, RightLeg]
-                 }
-tc2 = tc1
-tc2_5 = addCoev RightLoop $ addCoev (SecondHalf LeftLoop) $ addCoev LeftLeg $ addCoev LeftLoop tc2
-newV = take 4 $ vertices tc2_5
-tc3 = foldl f tc2_5 $ zipWith (,) newV $ tail newV
-  where f a (x,y)  = connect x y a
-tc4 = foldl f tc3 [1..3]
-  where f a _ = contract (edges a !! 1) a
+initialTC = TwoComplex { vertices = [Main, LeftPuncture, RightPuncture]
+                       , edges    = [LeftLoop, RightLoop, LeftLeg, RightLeg]
+                       , image    = id
+                       }
+            
+slide = do
+  (top1,lt1,_) <- addCoev LeftLoop
+  (top2,lt2,rt2) <- addCoev LeftLeg
+  (top3,rt13,lt3) <- addCoev (SecondHalf LeftLoop)
+  (top4,lt4,rt4) <- addCoev RightLoop
+  e1 <- connect top1 top2
+  e2 <- connect top2 top3
+  e3 <- connect top3 top4
+  contract e1
+  contract e2
+  contract e3
+  lt43 <- tensor lt4 lt3
+  lt42 <- tensor lt43 lt2
+  lt41 <- tensor lt42 lt1
+  contract rt4
+
+
+finalTC = execState slide initialTC
+
+
+-- tc4 = foldl f tc3 [1..3]
+--   where f a _ = contract (edges a !! 0) a
+-- e4 = edges tc4
+-- tc5 = tensor (e4) $ tensor (e4 !! 0) (e4 !! 3) tc4
